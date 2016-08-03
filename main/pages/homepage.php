420da51ab242b887e732848fe328ef6583f4b748
Carson Jones <carsonjones@Carsons-MacBook-Pro.local>
2016-08-03 14:42:53 -0500

    no message

---
+++ main/pages/homepage.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<HTML>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Fuji Sushi Bar | Auburn AL</title>
+
+    <!--build:css css/main.min.css-->
+    <link rel="stylesheet" href="../../bower_components/normalize-css/normalize.css">
+    <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/homepage.css" type="text/css">
+    <!--endbuild-->
+
+  </head>
+  <body>
+
+
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="../img/FujiLogoTransparentBG.png" alt="Dispute Bills">
+          </a>
+        </div>
+        <div id="navbar3" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Location</a></li>
+            <li><a href="#">Employment</a></li>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+      <!--/.container-fluid -->
+    </nav>
+
+    <section class="jumbo">
+      
+    </section>
+
+    <!--build:js /js/main.min.js -->
+    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../js/homepage.js"></script>
+    <!-- endbuild -->
+  </body>
+
+</HTML>
